7693f0782ae9b4f26a9f497235f896e54d1e3355
Lilian Hung <l.hung.m@gmail.com>
2022-05-15 08:53:47 +0200

    Add update, delete todo functions. Modify create function to create with incrementing todoId

--- src/app/todos.controller.ts
+++ src/app/todos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Delete, Put } from '@nestjs/common';
 import { CreateTodoDto } from './todos.dto';
 import { Todo } from './todos.interface';
 import { TodosService } from './todos.service';
@@ -8,8 +8,20 @@ export class TodosController {
   constructor(private todosService: TodosService) {}
 
   @Post()
-  async create(@Body() createTodoDto: CreateTodoDto) {
-    this.todosService.create(createTodoDto);
+  async create(@Body() createTodo: CreateTodoDto) {
+    this.todosService.create(createTodo);
+  }
+
+  @Put(':id')
+  async update(@Param() params, @Body() updateTodo: CreateTodoDto) {
+    // console.log('TodosController => update => params', params);
+    // console.log('TodosController => update => updateTodo', updateTodo);
+    this.todosService.update(Number(params.id), updateTodo);
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    this.todosService.delete(params.id);
   }
 
   @Get()
--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -4,7 +4,7 @@ import { Todo } from './todos.interface';
 @Injectable()
 export class TodosService {
   
-  private readonly todos: Todo[] = [{
+  private todos: Todo[] = [{
     title: 'first todo',
     description: 'just an example todo',
     id: 1,
@@ -13,12 +13,33 @@ export class TodosService {
     type: 1
   }];
 
+  private todoIdCounter = 1;
+
   create(todo: Todo) {
+    this.todoIdCounter ++; //TODO: change this when adding database
+    todo.id = this.todoIdCounter;
     console.log('new todo - push todo: ', todo);
+
     this.todos.push(todo);
   }
 
   findAll(): Todo[] {
     return this.todos;
+  }
+
+  update(id: number, todoUpdate: Todo){
+    this.todos.find(t => {
+      if(t.id === id){
+        console.log('t.id === id')
+        t.title = todoUpdate.title;
+        t.description = todoUpdate.description;
+      }
+    })
+    // console.log('TodosService => update => todoUpdate', todoUpdate);
+    // console.log('TodosService => update => this.todos', this.todos);
+  }
+
+  delete(id: number) {
+    this.todos = this.todos.filter(t => t.id != id);
   }
 }
