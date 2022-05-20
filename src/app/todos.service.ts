0bf362ee393841abdd7ef8efcb2c09eacf0febde
Lilian Hung <l.hung.m@gmail.com>
2022-05-20 09:48:25 +0200

    Refactor todoService to use fp-ts

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { Todo } from './todos.interface';
-
+import { filter } from 'fp-ts/Array'
+import { map } from 'fp-ts/Array';
+import { pipe } from 'fp-ts/lib/function';
+import { oneThroughThree } from './todos.dto';
 @Injectable()
 export class TodosService {
   
@@ -27,30 +30,59 @@ export class TodosService {
   }
 
   findOne(id: number): Todo {
-    return this.todos.find(t => t.id === id);
+    // return this.todos.find(t => t.id === id);
+    const filtered = filter(this.checkTodoForSameId(id))(this.todos);
+    return this.getObjFromArr(filtered);
   }
 
   update(id: number, todoUpdate: Todo) {
-    this.todos.find(t => {
-      if(t.id === id){
-        t.title = todoUpdate.title;
-        t.description = todoUpdate.description;
-        t.priority = todoUpdate.priority;
-        t.type = todoUpdate.type;
-      }
-    })
+    pipe(
+      this.todos, 
+      filter(this.checkTodoForSameId(id)), 
+      this.getObjFromArr, 
+      this.updateTodoObj(todoUpdate.title, todoUpdate.description, todoUpdate.priority, todoUpdate.type)
+    );
+    // this.todos.find(t => {
+    //   if(t.id === id){
+    //     t.title = todoUpdate.title;
+    //     t.description = todoUpdate.description;
+    //     t.priority = todoUpdate.priority;
+    //     t.type = todoUpdate.type;
+    //   }
+    // })
   }
 
   updateIsDone(id: number, isDone: boolean) {
-    // console.log("🚀 ~ file: todos.service.ts ~ line 45 ~ TodosService ~ updateIsDone ~ isDone", isDone)
-    this.todos.find(t => {
-      if(t.id === id){
-        t.isDone = isDone;
-      }
-    })
+    pipe(
+      this.todos, 
+      filter(this.checkTodoForSameId(id)),
+      map(this.changeIsDone(isDone)) 
+    );
+    // this.todos.find(t => {
+    //   if(t.id === id){
+    //     t.isDone = isDone;
+    //   }
+    // })
   }
 
   delete(id: number) {
-    this.todos = this.todos.filter(t => t.id != id);
+    // this.todos = this.todos.filter(t => t.id != id);
+    this.todos = filter(this.checkTodoForNotSameId(id))(this.todos);
   }
+
+  ///////// helper
+  private todoHasNotSameId = (t: Todo, id: number) => t.id != id;
+  private checkTodoForNotSameId = (id: number) => (t: Todo) => this.todoHasNotSameId(t, id);
+
+  private todoHasSameId = (t: Todo, id: number) => t.id === id;
+  private checkTodoForSameId = (id: number) => (t: Todo) => this.todoHasSameId(t, id);
+
+  private changeIsDone = (isDone: boolean) => (t: Todo) => t.isDone = isDone;
+  private updateTodoObj = (ti: string, d: string, p: number, ty: oneThroughThree) => (todo: Todo) => {
+    todo.title = ti;
+    todo.description = d;
+    todo.priority = p;
+    todo.type = ty;
+  }
+  private getObjFromArr = a => a[0];
 }
